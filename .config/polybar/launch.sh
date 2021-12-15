df9f860bb8cf74812ea9b324f73ccf299d5b04ff
Alfie Richardson <01richardsonalfie@protonmail.com>
2021-12-15 20:56:31 +0000

    Added name to launch.sh

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+#        _    ___ _       
+#       | |  / __|_)       Alfie Richardson
+#  _____| |_| |__ _ _____  
+# (____ | (_   __) | ___ | https://github.com/AlfieRichardson
+# / ___ | | | |  | | ____| https://gitlab.com/Penguinatic
+# \_____|\_)|_|  |_|_____)
+#
+# A launch script for my polybars.
+
 # Terminate any running instances.
 killall -q polybar
 
